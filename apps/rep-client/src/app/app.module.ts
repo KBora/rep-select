0d06dca14418e4a74ab9bc58b49777be0f526596
Kian Woo <kian.woo@gmail.com>
2021-07-12 21:23:10 +1000

    Add home and shared modules and routing

--- apps/rep-client/src/app/app.module.ts
+++ apps/rep-client/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    HomeModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
